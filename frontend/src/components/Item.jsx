8b67d3760d61f3a3d3a5b8437f23d1afa9193f60
Pranay Chandurkar <pranaychandurkar84@gmail.com>
2025-07-21 22:34:31 +0530

    cut extra space

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -5,13 +5,6 @@ const Item = ({category}) => {
     const { products } = useProduct();
   return (
     <div>
-      {/* {products.map((product)=>{
-        return <div className=""> 
-          <h3 className="text-lg font-semibold">{product.name}</h3>
-          <img src={product.image} alt={product.name} className="w-full h-auto mb-2" />
-        </div>
-      })} */}
-
       <h2 className='text-xl text-center font-medium mt-3'>Recent Work</h2>
       <div className="grid grid-cols-2 p-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {products.map((product, i) => {
